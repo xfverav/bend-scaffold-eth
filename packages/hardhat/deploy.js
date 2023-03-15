cf14c177e5f92f25c9f931db30803c035cd8d457
Xavier Vera <xfverav@gmail.com>
2023-03-14 21:41:49 -0400

    Initial commit

---
+++ packages/hardhat/deploy.js
@@ -0,0 +1,21 @@
+const hre = require("hardhat");
+
+async function main() {
+  const [deployer] = await hre.ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  const Token = await hre.ethers.getContractFactory("LendingPool");
+  const token = await Token.deploy();
+
+  await token.deployed();
+
+  console.log("YourToken deployed to:", token.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
